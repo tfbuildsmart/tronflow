7daf181c91693b3df77c65f074da3ef6be045702
Abdullah <abdullah@optimageeks.com>
2020-10-30 02:06:38 +0500

    tronflow fixes for invest

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,34 @@
+// on load
+$(window).on("load", function () {
+  const url = new URL(window.location);
+  const params = new URLSearchParams(url.search);
+  if (params.has("ref")) {
+    document.getElementById("reffererAddress").value = params.get("ref");
+  }
+});
 // onScroll
-$(window).on("scroll", function(){
-    var scrollTop = $(window).scrollTop();
-    if(scrollTop > 50){
-    $('#main-nav').addClass('fixed-top');
-    }
-    else{
-        $('#main-nav').removeClass('fixed-top');
-
-    }
-})
+$(window).on("scroll", function () {
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > 50) {
+    $("#main-nav").addClass("fixed-top");
+  } else {
+    $("#main-nav").removeClass("fixed-top");
+  }
+});
 
 // Accordion toggle icon
-$('.card-header').click(function () {
-    $(this).toggleClass('active');
+$(".card-header").click(function () {
+  $(this).toggleClass("active");
 });
 
 // Initialize popover
 $('[data-toggle="popover"]').popover();
 
 // Make popover dismissable
-$('.popover-dismiss').popover({
-    trigger: 'focus'
-  });
+$(".popover-dismiss").popover({
+  trigger: "focus",
+});
 
-window.setInterval(function(){
-    $(function() {
-        $(".custom-popover").toggleClass("custom-popover-active");
-  });
+window.setInterval(function () {
+  $(".custom-popover").toggleClass("custom-popover-active");
 }, 30000);
--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -1,66 +1,223 @@
-const abi =[{"outputs":[{"type":"uint256"}],"constant":true,"name":"devCommission","stateMutability":"View","type":"Function"},{"name":"withdraw","stateMutability":"Nonpayable","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"totalPayout","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"commissionDivisor","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"totalInvested","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"totalReinvest","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"getvel","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"inputs":[{"name":"_addr","type":"address"}],"name":"getProfit","stateMutability":"View","type":"Function"},{"outputs":[{"name":"trxDeposit","type":"uint256"},{"name":"time","type":"uint256"},{"name":"interestProfit","type":"uint256"},{"name":"affRewards","type":"uint256"},{"name":"payoutSum","type":"uint256"},{"name":"affFrom","type":"address"},{"name":"aff1sum","type":"uint256"},{"name":"aff2sum","type":"uint256"},{"name":"aff3sum","type":"uint256"},{"name":"aff4sum","type":"uint256"},{"name":"aff5sum","type":"uint256"},{"name":"aff6sum","type":"uint256"},{"name":"aff7sum","type":"uint256"},{"name":"aff8sum","type":"uint256"}],"constant":true,"inputs":[{"type":"address"}],"name":"players","stateMutability":"View","type":"Function"},{"payable":true,"inputs":[{"name":"_affAddr","type":"address"}],"name":"deposit","stateMutability":"Payable","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"totalPlayers","stateMutability":"View","type":"Function"},{"name":"reinvest","stateMutability":"Nonpayable","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"activedeposits","stateMutability":"View","type":"Function"},{"outputs":[{"type":"uint256"}],"constant":true,"name":"getContractBalanceRate","stateMutability":"View","type":"Function"},{"inputs":[{"name":"_marketingAddr","type":"address"},{"name":"_projectAddr","type":"address"}],"stateMutability":"Nonpayable","type":"Constructor"},{"payable":true,"stateMutability":"Payable","type":"Fallback"},{"inputs":[{"indexed":true,"name":"user","type":"address"},{"indexed":true,"name":"_referrer","type":"address"},{"name":"_time","type":"uint256"}],"name":"Newbie","type":"Event"},{"inputs":[{"indexed":true,"name":"user","type":"address"},{"name":"amount","type":"uint256"},{"name":"_time","type":"uint256"}],"name":"NewDeposit","type":"Event"},{"inputs":[{"indexed":true,"name":"user","type":"address"},{"name":"amount","type":"uint256"},{"name":"_time","type":"uint256"}],"name":"Withdrawn","type":"Event"},{"inputs":[{"indexed":true,"name":"referrer","type":"address"},{"indexed":true,"name":"referral","type":"address"},{"indexed":true,"name":"level","type":"uint256"},{"name":"amount","type":"uint256"},{"name":"_time","type":"uint256"}],"name":"RefBonus","type":"Event"},{"inputs":[{"indexed":true,"name":"user","type":"address"},{"name":"amount","type":"uint256"},{"name":"_time","type":"uint256"}],"name":"Reinvest","type":"Event"}]
+const abi = [
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "devCommission",
+    stateMutability: "View",
+    type: "Function",
+  },
+  { name: "withdraw", stateMutability: "Nonpayable", type: "Function" },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "totalPayout",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "commissionDivisor",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "totalInvested",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "totalReinvest",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "getvel",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    inputs: [{ name: "_addr", type: "address" }],
+    name: "getProfit",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [
+      { name: "trxDeposit", type: "uint256" },
+      { name: "time", type: "uint256" },
+      { name: "interestProfit", type: "uint256" },
+      { name: "affRewards", type: "uint256" },
+      { name: "payoutSum", type: "uint256" },
+      { name: "affFrom", type: "address" },
+      { name: "aff1sum", type: "uint256" },
+      { name: "aff2sum", type: "uint256" },
+      { name: "aff3sum", type: "uint256" },
+      { name: "aff4sum", type: "uint256" },
+      { name: "aff5sum", type: "uint256" },
+      { name: "aff6sum", type: "uint256" },
+      { name: "aff7sum", type: "uint256" },
+      { name: "aff8sum", type: "uint256" },
+    ],
+    constant: true,
+    inputs: [{ type: "address" }],
+    name: "players",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    payable: true,
+    inputs: [{ name: "_affAddr", type: "address" }],
+    name: "deposit",
+    stateMutability: "Payable",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "totalPlayers",
+    stateMutability: "View",
+    type: "Function",
+  },
+  { name: "reinvest", stateMutability: "Nonpayable", type: "Function" },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "activedeposits",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    outputs: [{ type: "uint256" }],
+    constant: true,
+    name: "getContractBalanceRate",
+    stateMutability: "View",
+    type: "Function",
+  },
+  {
+    inputs: [
+      { name: "_marketingAddr", type: "address" },
+      { name: "_projectAddr", type: "address" },
+    ],
+    stateMutability: "Nonpayable",
+    type: "Constructor",
+  },
+  { payable: true, stateMutability: "Payable", type: "Fallback" },
+  {
+    inputs: [
+      { indexed: true, name: "user", type: "address" },
+      { indexed: true, name: "_referrer", type: "address" },
+      { name: "_time", type: "uint256" },
+    ],
+    name: "Newbie",
+    type: "Event",
+  },
+  {
+    inputs: [
+      { indexed: true, name: "user", type: "address" },
+      { name: "amount", type: "uint256" },
+      { name: "_time", type: "uint256" },
+    ],
+    name: "NewDeposit",
+    type: "Event",
+  },
+  {
+    inputs: [
+      { indexed: true, name: "user", type: "address" },
+      { name: "amount", type: "uint256" },
+      { name: "_time", type: "uint256" },
+    ],
+    name: "Withdrawn",
+    type: "Event",
+  },
+  {
+    inputs: [
+      { indexed: true, name: "referrer", type: "address" },
+      { indexed: true, name: "referral", type: "address" },
+      { indexed: true, name: "level", type: "uint256" },
+      { name: "amount", type: "uint256" },
+      { name: "_time", type: "uint256" },
+    ],
+    name: "RefBonus",
+    type: "Event",
+  },
+  {
+    inputs: [
+      { indexed: true, name: "user", type: "address" },
+      { name: "amount", type: "uint256" },
+      { name: "_time", type: "uint256" },
+    ],
+    name: "Reinvest",
+    type: "Event",
+  },
+];
 let connection;
 let mainAccount;
-let contractAddress = "TRktZxNpTmbFEchoQtj8U5fpk9Xn42ZnkQ"; // Muzammmil Testing
+const contractAddress = "TRktZxNpTmbFEchoQtj8U5fpk9Xn42ZnkQ";
 
 window.addEventListener("message", (e) => {
-  setInterval(async function checkConnection() {
-    if (
-      window.tronWeb &&
-      window.tronWeb.defaultAddress &&
-      window.tronWeb.defaultAddress.base58 === "undefined"
-    ) {
-      connection = "TROn LINK is not available";
-      jQuery("#metamaskConnection").text(connection);
-    } else {
-      connection = "Connected to Tron LINK.";
-      jQuery("#metamaskConnection").text(connection);
-
-      mainAccount = await window.tronWeb.defaultAddress.base58;
-      jQuery(document).ready(function () {
-        isLocked();
-        getBalanceOfAccount();
-      });
-    }
-
-    if (e.data.message && e.data.message.action == "setNode") {
-      console.log("setNode event", e.data.message);
-      if (e.data.message.data.node.chain == "_") {
-        console.log("tronLink currently selects the main chain");
-      } else {
-        console.log("tronLink currently selects the side chain");
-      }
-    }
-  }, 3000);
-
-  if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
-    // clearInterval(obj)
-    let tronweb = window.tronWeb;
-  }
+  checkConnection(e);
 });
-//----------------//
-async function isLocked() {
-  if (window.tronWeb.defaultAddress.base58 == null) {
-    console.log(err);
-    jQuery("#lock").text(err);
-  } else if (window.tronWeb.defaultAddress.base58 === 0) {
-    jQuery("#lock").text("TRON LINK is locked.");
+
+// ---------------//
+async function checkConnection(e) {
+  if (window.tronWeb?.defaultAddress?.base58) {
+    showPopup("Connected to Tron LINK.");
+
+    mainAccount = window.tronWeb.defaultAddress.base58;
+    jQuery(document).ready(function () {
+      document.getElementById("accoutRef").value =
+        window.location.hostname + "?ref=" + mainAccount;
+      getBalanceOfAccount();
+    });
   } else {
-    jQuery("#lock").text("TRON LINK is unlocked.");
+    showPopup("Tron LINK is not available");
+  }
+
+  if (e.data.message && e.data.message.action == "setNode") {
+    console.log("setNode event", e.data.message);
+    if (e.data.message.data.node.chain == "_") {
+      console.log("tronLink currently selects the main chain");
+    } else {
+      console.log("tronLink currently selects the side chain");
+    }
   }
 }
+//----------------//
+// async function isLocked() {
+//   if (window.tronWeb.defaultAddress.base58 == null) {
+//     console.log(err);
+//     jQuery("#lock").text(err);
+//   } else if (window.tronWeb.defaultAddress.base58 === 0) {
+//     jQuery("#lock").text("TRON LINK is locked.");
+//   } else {
+//     jQuery("#lock").text("TRON LINK is unlocked.");
+//   }
+// }
 //----------------//
 async function getBalanceOfAccount() {
   tronWeb.trx.getBalance(mainAccount, function (err, res) {
-    jQuery("#getBalance").text(res / 1000000);
+    let balance = res / 1000000;
+    jQuery("#getBalance").text(parseInt(balance));
   });
 }
 async function deposit() {
+  // TTDKQAFBuRg52wC6dtrnnMti7HTNjqCo1v
   let address = jQuery("#reffererAddress").val();
   let amount = jQuery("#depositamount").val();
-  if(amount<50){
-    jQuery("#transferToken").text("minimm Amount is 50 TRX!");
-  }else{
+  if (address.length < 10) {
+    showPopup("Please Enter Right Address");
+  } else if (amount < 50) {
+    showPopup("Minimum Amount is 50 TRX");
+  } else {
     let contract = await tronWeb.contract().at(contractAddress);
     contract
       .deposit(address)
@@ -68,8 +225,7 @@ async function deposit() {
         callValue: tronWeb.toSun(amount),
       })
       .then((output) => {
-        console.log("- Output:", output, "\n");
-        jQuery("#transferToken").text("Hash ID:" + " " + output);
+        console.log("Hash ID:", output, "\n");
       });
   }
 }
@@ -80,7 +236,7 @@ async function withdraw() {
     .withdraw()
     .send()
     .then((output) => {
-      jQuery("#withdraw").text("HashId:" + " " + output);
+      console.log("HashId:" + " " + output);
     });
 }
 //reinvest your fund!
@@ -90,6 +246,15 @@ async function reinvest() {
     .reinvest()
     .send()
     .then((output) => {
-      jQuery("#reinvest").text("HashId:" + " " + output);
+      console.log("HashId:" + " " + output);
     });
+}
+
+function showPopup(error) {
+  $("#popup-error-msg").html(error);
+
+  $(".error-popover").toggleClass("show");
+  window.setTimeout(function () {
+    $(".error-popover").toggleClass("show");
+  }, 5000);
 }
