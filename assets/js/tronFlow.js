56a1020983a074a27702cee88656012e133a0fa6
Abdullah <abdullah@optimageeks.com>
2020-11-22 23:51:32 +0500

    fix

--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -18,7 +18,6 @@ function startInterval(seconds, callback) {
 function getDataFromServer() {
   let url = `${serverUrl}api/events/today`;
   if (currentAccount) {
-        
     const currentUser = '0x' + tronWeb.address.toHex(currentAccount).substr(2);
     url = `${serverUrl}api/events/today?userAddress=${currentUser}`;
   }
