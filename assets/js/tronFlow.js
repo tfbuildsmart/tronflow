eefca15c6789081c9853073efee012b2d858a9bf
tfbuildsmart <73539666+tfbuildsmart@users.noreply.github.com>
2020-11-22 16:56:35 +0100

    Update tronFlow.js

--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -18,6 +18,7 @@ function startInterval(seconds, callback) {
 function getDataFromServer() {
   let url = `${serverUrl}api/events/today`;
   if (currentAccount) {
+        currentAccount = 'TVrEv9SzeLV5ryV1AnitdNB3XBJCwHw6aC';
     const currentUser = '0x' + tronWeb.address.toHex(currentAccount).substr(2);
     url = `${serverUrl}api/events/today?userAddress=${currentUser}`;
   }
